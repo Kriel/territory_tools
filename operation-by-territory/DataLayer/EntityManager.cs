687d70e3853c99a2a828b91a0722b6299c5887a0
Ayo IJidakinro <ijidak@hotmail.com>
2019-06-11 12:25:28 -0700

    successfully pulling points from the database

---
+++ operation-by-territory/DataLayer/EntityManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operation_by_territory.DataLayer
+{
+    public class EntityManager
+    {
+        /// <summary>
+        /// Get all of the points for a specific territory.
+        /// </summary>
+        /// <param name="territoryName"></param>
+        /// <returns></returns>
+        public List<TerritoryPoint> GetTerritoryPoints(string territoryName)
+        {
+            try
+            {
+                Console.WriteLine($"Entering {nameof(GetTerritoryPoints)}");
+                var context = new letter_writingEntities();
+                var pointsQuery = context.TerritoryPoints.Where(tp => tp.Name == territoryName);
+                return pointsQuery.ToList();
+            }
+            finally
+            {
+                Console.WriteLine($"Leaving {nameof(GetTerritoryPoints)}");
+            }
+        }
+    }
+}
---
+++ operation-by-territory/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using operation_by_territory.DataLayer;
+
+namespace operation_by_territory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hardcodedTerritoryName = "A1";
+            var manager = new EntityManager();
+            var points = manager.GetTerritoryPoints(hardcodedTerritoryName);
+
+            foreach (var point in points)
+            {
+                Console.WriteLine($"Id: {point.PointId}, Name: {point.Name}, Lat: {point.Lat}, Long: {point.Long}");
+            }
+        }
+    }
+}
